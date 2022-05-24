24b5b8a558839df52e8d98fc9c82db712b4446ab
Jonah Nimijean <jnimijean@me.com>
2022-05-24 00:23:33 -0400

    Initial Commit

---
+++ FrequencyQuiz/FrequencyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrequencyQuizApp.swift
+//  FrequencyQuiz
+//
+//  Created by Jonah Nimijean on 2022-05-24.
+//
+
+import SwiftUI
+
+@main
+struct FrequencyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
